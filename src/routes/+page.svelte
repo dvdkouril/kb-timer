97eb03b0b81f6b430de7941a6f67af10d0e9f4f7
David Kouril <david.kouril@gmail.com>
2025-07-06 20:35:37 -0400

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,31 +26,79 @@
 	let tick = $state(0);
 	let countdownSeconds = $state(0);
 	let countdownStarted = $state(false);
+	let timerId: number | undefined = undefined;
+	let roundsFinished = $state("");
 
 	const handleClick = () => {
 		prompt = "starting the timer.";
 		countdownStarted = true;
 		countdownSeconds = setTime.minutes * 60 + setTime.seconds;
-		setInterval(() => {
+		roundsFinished = "";
+		if (timerId) {
+			clearInterval(timerId);
+		}
+		timerId = setInterval(() => {
 			tick = tick + 1;
 			countdownSeconds -= 1;
+
+			if (countdownSeconds === 0) {
+				roundsFinished += "✅";
+				countdownSeconds = setTime.minutes * 60 + setTime.seconds;
+			}
 		}, 1000);
 	};
 </script>
 
-<input bind:value={setsGoal} />
-<button on:click={handleClick}> start </button>
+<div id="form">
+	<label>
+		Sets:
+		<input bind:value={setsGoal} />
+	</label>
+	<label>
+		Time:
+		<input bind:value={timeGoal} />
+	</label>
+	<button on:click={handleClick}> start </button>
+</div>
 
 <div>
-	{setsGoal} sets in {timeGoal} minutes. <br />
-	{setTime.minutes}:{setTime.seconds} per set. <br />
-	{totalTime} total.
-</div>
-<div>
-	{prompt}
+	<span class="hl">{setsGoal}</span> sets in
+	<span class="hl">{timeGoal}</span>
+	minutes:
+	<span class="hl">{setTime.minutes}:{setTime.seconds}</span> per set. ({totalTime}
+	total.)
 </div>
 {#if countdownStarted}
-	<div>
-		{countdownSeconds}
+	<div id="running-countdown-info">
+		<div>
+			{roundsFinished}
+		</div>
+		<div id="countdown-number">
+			{countdownSeconds}
+		</div>
 	</div>
 {/if}
+
+<style>
+	p,
+	div {
+		font-size: 1.5em;
+		font-family: Georgia, "Times New Roman", Times, serif;
+	}
+
+	#form {
+		display: flex;
+		width: 200px;
+	}
+
+	#form {
+	}
+	#countdown-number {
+		font-size: 20em;
+	}
+
+	.hl {
+		background-color: red;
+		color: white;
+	}
+</style>
